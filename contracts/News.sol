a7f02e5fa1d1accfd545072ae9c67b8f4a36c83f
Prasoon Soni <prasoonsoni03@gmail.com>
2022-03-21 13:12:33 +0530

    Added Smart Contract

--- contracts/News.sol
+++ contracts/News.sol
@@ -2,5 +2,20 @@ pragma solidity ^0.5.0;
 
 contract News{
     uint public newsCount = 0;
+    struct NewsInfo {
+        uint id;
+        string title;
+        string description;
+        string publisher;
+        string dateCreated;
+    }
+
+    mapping(uint => NewsInfo) public newsInfos;
+
+    function createNews(string memory _title, string memory _description, string memory _publisher, string memory _timeCreated) public {
+        newsCount += 1;
+        newsInfos[newsCount] = NewsInfo(newsCount, _title, _description, _publisher, _timeCreated);
+    }
+
 }
 
